9640c5a4ab508c8edb306f5253ab16e9871db504
Siegerus <ivanoffsiegerus@gmail.com>
2025-09-10 19:35:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './assets/images/logo.svg';
+/* import logo from '/images/logo.svg' */
+import './App.scss';
+
+function App() {
+	return (
+		<div className="app">
+			<img src={logo} alt="" />
+		</div>
+	);
+}
+
+export default App;
+
