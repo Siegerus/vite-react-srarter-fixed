c1fb57ee34d8a2e957d868465f836ba87dbdedcc
Siegerus <ivanoffsiegerus@gmail.com>
2026-03-18 10:32:13 +0300

    fix starter files extentions/refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 import './App.scss';
 import Sandbox from './components/Sandbox';
 
 function App() {
-	return <Sandbox />;
+	return (
+		<Routes>
+			<Route path={'/'} element={<Sandbox />} />
+		</Routes>
+	);
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<App />
+	<React.StrictMode>
+		<BrowserRouter
+			future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+		>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
